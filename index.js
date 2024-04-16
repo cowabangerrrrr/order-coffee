9d38ee9c7a05a3111a015abee01027702f112e7b
Stas <stanislav.ivanov.2004@internet.ru>
2024-04-16 18:49:48 +0500

    Add Table

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function addEventCloseButton(node) {
     })
 }
 
+dict = {'обычном молоке': 'обычное', 'обезжиренном молоке': 'обезжиренное',
+    'соевом молоке' : 'соевоe', 'кокосовом молоке' : 'кокосовое',
+    'взбитых сливок': 'взбитые сливки', 'зефирок': 'зефирки',
+    'шоколад' : 'шоколад', 'корицу' : 'корица'}
 addEventCloseButton(document);
 
 const addButton = document.querySelector('.add-button')
@@ -17,6 +21,10 @@ addButton.addEventListener('click', () => {
     const newNode = fieldsets[0].cloneNode(true);
 
     newNode.querySelector('.beverage-count').textContent = `Напиток №${++number}`;
+    const radios = newNode.querySelectorAll("input[type='radio']");
+    for(radio of radios){
+        radio.name = `milk${number}`;
+    }
     addEventCloseButton(newNode);
     fieldset.after(newNode);
 })
@@ -37,6 +45,7 @@ submitButton.addEventListener('click', (event) => {
         : (num > 20 && (mod === 2 || mod === 3 || mod === 4)) || (num >= 2 && num <= 4) ?
             'напитка'
             : 'напитков'}`;
+    addTable();
 })
 
 const closeNodeButton = document.querySelector('.close-modal');
@@ -44,3 +53,50 @@ closeNodeButton.addEventListener('click', () => {
     let overlay = document.querySelector('.overlay');
     overlay.style.visibility = 'hidden';
 })
+
+function addTable(){
+    const closeNodeButton = document.querySelector('.body-table');
+    closeNodeButton.innerHTML ="";
+    const listFieldSets = document.querySelectorAll('.beverage')
+    for (var fieldSet of listFieldSets){
+        const selectElement = fieldSet.querySelector("select");
+        const nameCoffe = selectElement.options[selectElement.selectedIndex].textContent;
+        let typeMilk = undefined;
+
+        var radioInputs = fieldSet.querySelectorAll("input[type='radio']");
+
+        for (var i = 0; i < radioInputs.length; i++) {
+            if (radioInputs[i].checked) {
+                typeMilk = dict[radioInputs[i].parentNode.querySelector("span").textContent];
+            }
+        }
+
+
+        var checkboxInputs = fieldSet.querySelectorAll("input[type='checkbox']");
+        var selectedValues = [];
+        for (var i = 0; i < checkboxInputs.length; i++) {
+            if (checkboxInputs[i].checked) {
+                var selectedValue = dict[checkboxInputs[i].parentNode.querySelector("span").textContent];
+                selectedValues.push(selectedValue);
+            }
+        }
+
+
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        td1.textContent = nameCoffe;
+
+        let td2 = document.createElement("td");
+        td2.textContent = typeMilk;
+
+        let td3 = document.createElement("td");
+        td3.textContent = selectedValues.join(", ");
+
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        closeNodeButton.append(tr);
+    }
+
+}
+
